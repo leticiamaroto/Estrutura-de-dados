140322b22d5ecf14875c15aa0a2b91c749106379
letícia <leticiamarotods01@outlook.com>
2022-01-23 20:03:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome = 'Letícia';
+console.log(nome)
+
+let numero = 21;
+console.log(numero)
+
+let ano = 2022 - (numero)
+console.log(ano)
+
+let numerosSomados = 2022 + 21;
+console.log(numerosSomados)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = false;
+console.log(false)
+
+console.log(typeof quartaFeira)
